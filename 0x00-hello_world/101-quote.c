af601daea4b6a325c349a34c35e4ab0e748a57bf
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-06-02 14:19:34 -0700

    qoute

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	char string[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char string[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	fwrite(2, string, 59);
 	return 1;
 }

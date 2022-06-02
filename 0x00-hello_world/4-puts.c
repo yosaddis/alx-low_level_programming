b8f2af5741a0fc7f02a87b6b135dc4f7be70bc99
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-06-02 12:57:44 -0700

    first c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * Code discription here
+ *
+ * */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

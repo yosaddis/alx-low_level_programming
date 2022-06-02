71180dde9f095d2d0935577cca3c9a8b9be0843a
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-06-02 14:11:11 -0700

    qoute

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: 1 Always (Success)
+ */
+int main(void)
+{
+	char string[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, string, 59);
+	return 1;
+

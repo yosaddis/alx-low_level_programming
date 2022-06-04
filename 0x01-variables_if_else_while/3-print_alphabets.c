8ffb763aeef42d44e3904c90d98c75edfc11904e
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-06-03 22:21:57 -0700

    q3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0  (success)
+ *
+ */
+int main(void)
+{
+	char c;
+	int i = 0;
+
+	for (i = 97; i < 123; i++)
+	{
+		c = i;
+		putchar(c);
+	}
+	for (i = 65; i < 91; i++)
+	{
+		c = i;
+		putchar(c);
+	}	
+	putchar('\n');
+
+	return (0);
+}

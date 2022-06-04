0c59a69eeffc131b0588269103fd5f1677253925
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-06-04 05:25:42 -0700

    q3

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0  (success)
+ *
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}

55689086a91b0d207cb17e4a6ac984dce1377ef2
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-06-04 17:09:41 -0700

    3-combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0  (success)
+ *
+ */
+int main(void)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			for (k=48; k < 58; k++)
+			{
+				if (i != j && i != k && i != k && i < j && i < k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (j == 55 && i == 56 && k == 57)
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

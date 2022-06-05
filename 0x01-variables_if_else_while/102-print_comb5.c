e11b129b6c27d5d5ce3715567268a9be12a91fb5
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-06-04 18:06:25 -0700

    twotwodigits)

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0  (success)
+ *
+ */
+int main(void)
+{
+	int i = 48;
+	int j = 48;
+	int k = 48;
+	int l = 48;
+	int pair_01,pair_02;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			for (k = 48; k < 58; k++)
+			{
+				for (l = 48; l < 58; l++)
+				{
+					pair_01 = (i * 10) + j;
+					pair_02 = (k * 10) + l;		
+					if (pair_01 < pair_02)
+					{
+						putchar(i);
+						putchar(j);
+						putchar(' ');
+						putchar(k);
+						putchar(l);
+						if (i == 57 && j == 56 && k == 57 && i == 57)
+						{
+							break;
+						}
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

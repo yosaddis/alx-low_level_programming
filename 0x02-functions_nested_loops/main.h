8536a64a42ede3bc21058685c17feee73c8afb2c
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-06-07 05:31:35 -0700

    putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,9 +6,9 @@
  *
  * Return: On suceess 1.
  * On error, -1 is returned, and errno is set appropriately.
- *
-*int _putchar(char c)
-*{
+ */
+int _putchar(char c);
+/**{
 *	return(write(1, &c, 1));
 *}
 */

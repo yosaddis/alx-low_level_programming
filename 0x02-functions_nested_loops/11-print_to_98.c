50086d8610eb0404b5146ada11d87732dfbee351
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-06-07 13:31:02 -0700

    n natural

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,7 @@ void print_to_98(int n)
 
 	if (n < 98)
 	{
-		for (; i <= 98; i++)
+		for (; n < 98; i++)
 		{
 			_putchar(i);
 			_putchar(',');
@@ -22,7 +22,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (; i > 98; i--)
+		for (; n > 98; i--)
 		{
 			_putchar(i);
 			_putchar(',');

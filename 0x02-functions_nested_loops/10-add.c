10ae788a27be96c2e0b6c133cebf2d5bfb8a8fb0
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-06-07 13:08:31 -0700

    times table

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include "main.h"
+#include<ctype.h>
+/**
+ * add - adds two numbers
+ * Return: 0 (success)
+ *@a:a
+ *@b:b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -17,6 +17,8 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
+int add(int,int);
+void print_to_98(int n);
 /**{
 *	return(write(1, &c, 1));
 *}

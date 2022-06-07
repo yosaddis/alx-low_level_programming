e78536f1131a4a5cf8ebfbc5b89c65ba5ba75774
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-06-07 12:35:00 -0700

    jack

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include "main.h"
+#include<stdlib.h>
+#include<limits.h>
+/**
+ * jack_bauer - shows all digital times
+ * Return: 0 (success)
+ */
+
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 48; i < 50; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			for(k = 48; k <= 53;k++)
+			{
+				for (l = 48; k <= 57; k++)
+				{
+					if (i >= 50 && j >= 52)
+						break;
+					_putchar(i);
+					_putchar(j);
+					_putchar(':');
+					_putchar(k);
+					_putchar(l);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}

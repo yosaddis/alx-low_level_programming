df476e16848877dd0a52c45e8540156ea377955c
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-06-07 13:01:55 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include "main.h"
+#include<stdlib.h>
+#include<limits.h>
+/**
+ * times_table - shows all digital times
+ * Return: 0 (success)
+ */
+
+void times_table(void)
+{
+	int i, j, product;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(48);
+		for (j = 1; j <= 9; j++)
+		{
+			product = i * j;
+			_putchar(44);
+			_putchar(32);
+			if(product <= 9)
+			{
+				_putchar(32);
+				_putchar(product + 48);
+			}
+			else
+			{
+				_putchar((product / 10) + 48);
+				_putchar((product % 10) + 48);
+			}
+		}
+		_putchar('\n');
+	}
+}

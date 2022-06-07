384bc9a495085be81d8b41327354ce7ad350bfff
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-06-07 12:11:38 -0700

    last digit correction

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,7 +11,6 @@
 int print_last_digit(int n)
 {
 	int last;
-	
 	last = n % 10;
 	if (last < 0)
 	{
@@ -21,6 +20,6 @@ int print_last_digit(int n)
 	else
 	{
 		_putchar(last + 48);
-		return(last);
+		return (last);
 	}
 }

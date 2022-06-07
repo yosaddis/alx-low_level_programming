fe2c2e652f75e1ae5686061eba5d45da1f4de803
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-06-07 08:01:22 -0700

    space in a comment. comeon!

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,16 +3,14 @@
 #include<ctype.h>
 /**
  * _islower - checks if letter is lower case
- *
  * @c: charachter 
- * 
  * Return: 0 (success)
  *
  */
 
 int _islower(int c)
 {
-	if(islower(c) != 0)
+	if (islower(c) != 0)
 		return (1);
 	return (0);	
 }

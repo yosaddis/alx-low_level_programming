b45ec3c64ae4fe7326cf9abf62e608f765260ca0
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-06-07 11:10:39 -0700

    print sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include "main.h"
+#include<ctype.h>
+/**
+ * print_sign - checks a number sign and prints
+ * Return: 0 (success)
+ *@c:c
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if(n < 0)
+	{
+		_putchar('-');
+		return(-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}

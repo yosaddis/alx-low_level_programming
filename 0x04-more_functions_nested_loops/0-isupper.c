e309449b1340ea55a2dfd91644d87f3c4dc8ce29
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-06-09 04:26:20 -0700

    changed

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include <ctype.h>
-
 /**
  * _isupper - check fors upper letter
  * Return: 1 on success
@@ -9,7 +6,7 @@
 
 int _isupper(int c)
 {
-	if (isupper(c) == 0)
+	if (c >= 'A' && c <= 'Z')
 		return (0);
 	return (1);
 }

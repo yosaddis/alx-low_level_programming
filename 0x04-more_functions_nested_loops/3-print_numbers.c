6c9e343f45df43adbf2bde0e7c1df21f450603c5
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-06-09 05:27:43 -0700

    print numbers added

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+/**
+ * print_numbers - prints digits
+ * Return: 1 on success
+ */
+
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+

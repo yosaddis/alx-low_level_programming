a87a07f71fe1a111d0816ea05164025db976d90f
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-06-09 14:43:13 -0700

    print triangle works

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_triangle - print square of hash sign
+ * Return: 1 on success
+ * @n: number of print
+ */
+
+void print_triangle(int n)
+{
+	int i, j, k;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = i; j < n; j++)
+			{
+				_putchar(' ');
+			}
+			for (k = 0; k <= i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
+

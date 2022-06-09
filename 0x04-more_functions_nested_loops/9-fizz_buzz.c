afc490721a30edf061ed3c11df9f056df0084932
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-06-09 13:53:12 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,11 +12,11 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (((i % 3) == 0) && ((i % 5) == 0))
-			printf("fizzBuzz");
+			printf("FizzBuzz");
 		else if ((i % 3) == 0)
-			printf("fizz");
+			printf("Fizz");
 		else if ((i % 5) == 0)
-			printf("buzz");
+			printf("Buzz");
 		else
 			printf("%d", i);
 		printf(" ");

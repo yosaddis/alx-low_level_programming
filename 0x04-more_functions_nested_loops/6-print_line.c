f5efe8178724044d0ce123cc6e32de3b3ba2a1aa
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-06-09 12:36:09 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - prints line
+ * Return: 1 on success
+ */
+
+void print_line(int n)
+{
+	char i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
+

773eff8b3516b0e5dfa78093ef312dfdf4badf1a
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-06-09 04:20:43 -0700

    main.h is added

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/**
+ * _isupper - checks for upper cases
+ * Return: 1 for uppper other for lower case
+ * @c: given charachter
+ */
+int _isupper(int c);
+
+#endif /* MAIN_H */

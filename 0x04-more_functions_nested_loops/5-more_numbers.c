ede94a2234c91d2cee222e677efe63236c59d0e0
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-06-09 07:03:59 -0700

    added 5-main

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,18 +8,22 @@ void more_numbers(void)
 {
 	char i;
 	int j;
+	int k;
 
-	for (j = 0; j <= 14; j++)
+	for (k = 0; k < 10; k++)
 	{
-		if (j == 10)
-			i = '0';
-		for (i = '0'; i <= '9'; i++)
+		for (j = 0; j <= 14; j++)
 		{
-			if (j > 9)
+			if (j == 10)
+				i = '0';
+			for (i = '0'; i <= '9'; i++)
 			{
-				_putchar('1');
+				if (j > 9)
+				{
+					_putchar('1');
+				}
+				_putchar(i);
 			}
-			_putchar(i);
 		}
 		_putchar('\n');
 	}

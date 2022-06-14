436ff0e9c8e593900c92112d744fbb15febfc248
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-06-14 13:22:34 -0700

    print upt to n

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_array -print array elements
+ * Return: void
+ * @a: pointer to string
+ * @n: numbers of chars to print
+ */
+
+void print_array(int *a, int n)
+{
+	int count = strlen(str);
+	int i;
+	int n;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", *(a + 1));
+		if (i != (n - 1))
+			printf(", ");
+	}
+	_putchar('\n');
+}

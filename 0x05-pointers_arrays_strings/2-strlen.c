36277eb69256294d5f1179d55822600e3edbff95
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-06-14 08:15:08 -0700

    strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,10 @@
 #include "main.h"
 #include <string.h>
+
 /**
  * _strlen - returns the size of string
  * Return: size of a string
- * @s: given string; 
+ * @s: given string
  */
 
 int _strlen(char *s)

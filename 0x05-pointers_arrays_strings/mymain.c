cf710bb2b4cc844be3363d9cac4646ae4890e734
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-06-15 00:03:58 -0700

    print array

--- 0x05-pointers_arrays_strings/mymain.c
+++ 0x05-pointers_arrays_strings/mymain.c
@@ -13,6 +13,15 @@ int main(void)
 	//printf("%s\n", str);
 	//rev_string(str);
 	//printf("%s\n",str);
-	puts_half(str);
+//	puts_half(str);
+//
+	int array[5];
+
+	array[0] = 98;
+	array[1] = 402;
+	array[2] = -198;
+	array[3] = 298;
+	array[4] = -1024;
+	print_array(array, 5);
 	return (0);
 }

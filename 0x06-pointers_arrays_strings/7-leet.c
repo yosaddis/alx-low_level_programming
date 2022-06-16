4a2526f0c9a7b1f8b6f5f3a03c30a1f92e2ea447
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-06-16 13:18:39 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+/**
+ * leet - changes string to leet style
+ * Return: char
+ * @s: pointer to the string to get leet
+ */
+
+char *leet(char *s)
+{
+	int i, count;
+	int small_letters[] = {97, 101, 111, 116, 108};
+	int capital_leters[] = {65, 69, 79, 84, 76};
+	int leet_numbers[] = {52, 51, 48, 55, 49};
+
+	while (*(s + count) != '\0')
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*(s + count) == capital_letters[i] || *(s + count) == small_letters[i])
+			{
+				*(s + count) = numbers[i];
+				break;
+			}
+		}
+		count++;
+	}
+	return (s);
+}

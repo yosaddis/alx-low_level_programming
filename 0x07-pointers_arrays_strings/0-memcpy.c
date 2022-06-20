b16ff3b1f73f60cdfc8bf092dc3c3465d4b62caa
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-06-20 02:06:49 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/0-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy- copies the content of a memory
+ * Return: dest
+ * @dest: pointer to destination string
+ * @src: pointer to source string
+ * @n: size to copy to
+ */
+
+char *_memcpy(char *dest, char *src, usigned int n)
+{
+	char *pszDest = (char *) dst;
+	const char *pszSource = (const char *)src;
+
+	if ((pszDest != NULL) && (pszSource != NULL))
+	{
+		*(pszDest++) = *(pszSource++);
+			--cnt;
+	return (dest);
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+#endif /* MAIN_H */

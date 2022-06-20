55f42a67c11d0eaf7b7eb38d9eafe72fd1152907
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-06-20 03:06:44 -0700

    strchr

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,6 +5,6 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_memset(char *s, char b, unsigned int n);
-char *strchr(char *s, char c);
+char *_strchr(char *s, char c);
 
 #endif /* MAIN_H */

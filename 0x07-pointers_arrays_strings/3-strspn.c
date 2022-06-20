be1a8303fb3b357afa0b9d84c9bba8f387e9891c
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-06-20 03:14:33 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strspn - gets the length of prefix
+ * Return: pointer to the memory area
+ * @s: pointer to string
+ * @accept: no of bytes on initial segement
+ */
+
+char *_strspn(char *s, char *accept)
+{
+	return (strspn(s, accpet));
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,5 +6,6 @@
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_memset(char *s, char b, unsigned int n);
 char *_strchr(char *s, char c);
+char *_strspn(char *s, char *accept);
 
 #endif /* MAIN_H */

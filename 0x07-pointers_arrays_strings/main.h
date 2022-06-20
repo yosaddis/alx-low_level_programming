3ce621bff037bcb7e5482aaa2e4861b1b23eb3d7
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-06-20 03:39:41 -0700

    strstr

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,5 +8,7 @@ char *_memset(char *s, char b, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+
 
 #endif /* MAIN_H */

0c6f51b9fed941bb792117cdce52a148e8606a5a
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-06-23 13:33:22 -0700

    sqrt

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,9 +10,9 @@ int is_prime_number(int n)
 	int i;
        
 	i = n / 2;
-	if ((i) == 1)
+	if (i == 1)
 		return (1);
-	else if (n % (i) == 0)
+	else if ((n % i) == 0)
 		return (0);
 	else
 		is_prime_number(n - 1);

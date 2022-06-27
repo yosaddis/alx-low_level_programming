bc3c5aecd2720b35b169298e54dc9342bc273bac
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-06-27 07:19:58 -0700

    add numbers by args

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+/**
+ * main - adds to postive numbers
+ * Return: 0 on success
+ * @argc: number of arguments
+ * @argv: array of inputs
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc == 1)
+		printf("0\n");
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]))
+				sum += atoi(argv[i]);
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}

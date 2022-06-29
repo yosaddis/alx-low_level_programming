4f3ace01b6afbdf6f29460c501aa24df7b0ea89c
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-06-29 12:30:35 -0700

    add by args

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 		{
 			for (j = 0; argv[i][j] != '\0'; j++)
 			{
-				if (isdigit(argv[i]))
+				if (isdigit(argv[i][j]))
 				{
 					printf("Error\n");
 					return (1);

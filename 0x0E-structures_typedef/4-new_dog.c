67c1d780243421971f044df27d33636694c545b7
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-07-04 09:52:17 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,16 +12,24 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d = malloc(sizeof(d));
+	int name_size, owner_size;
+	int i, j;
 
 	if (d != NULL || (name) || (owner))
 	{
 		d->name = malloc(sizeof(strlen(name)) + 1);
 		d->owner = malloc(sizeof(strlen(owner)) + 1);
-		
-		d->name = memcpy(d->name, &name, sizeof(name));
-		d->owner = memcpy(d->owner, &owner,sizeof(owner));
+		for (name_size = 0; name[name_size] != '\0'; name_size++)
+			;
+		for (owner_size = 0; owner[owner_size] != '\0'; owner_size++)
+			;
+		for (i = 0; i < name_size; i++)
+			d->name[i] = name[i];
+		d->name[i] = '\0';
+		for (j = 0; j < owner_size; j++)
+			d->owner[j] = owner[j];
+		d->owner[j] = '\0';
 		d->age = age;
-
 		return (d);
 	}
 	free(d);

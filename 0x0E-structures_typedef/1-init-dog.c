3f28ee8875985f40a9f024e00a8192c928cb7f59
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-07-04 03:50:50 -0700

    init dogo

--- 0x0E-structures_typedef/1-init-dog.c
+++ 0x0E-structures_typedef/1-init-dog.c
@@ -7,9 +7,9 @@
  * @owner: the owner of the dog
  * Return: void
  */
-void init_dog(struct dog *d, char *name , char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d.name = name;
-	d.age = age;
-	d.owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }

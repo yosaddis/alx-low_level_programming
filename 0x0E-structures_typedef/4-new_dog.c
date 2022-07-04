2f8a83ba6c67ee55f6c25ef981ba215fb5c49db5
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-07-04 05:08:55 -0700

    get a dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,18 @@
+#include"dog.h"
+/**
+ * new_dog - initializes new dog from struct
+ * @name: name to assign
+ * @age: the age
+ * @owner: the owner of the dog
+ * Return: pointer to the new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+
+	return (d);
+}

1f69554effd7580b8ea4e267d2e3360560b477f2
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-07-04 04:01:12 -0700

    dog init

---
+++ 0x0E-structures_typedef/_main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include"dog.h"
+#include"main.h"
+
+int main(void)
+{
+	struct dog my_dog;
+
+	init_dog(&my_dog, "poppy",3.5,"bob");
+	printf("My name is %s, and I am %.1f :) - woof \n",my_dog.name, my_dog.age);
+	return (0);
+}

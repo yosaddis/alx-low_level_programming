cea27f9b8475b16c599d9d2fd6175b241d22927c
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-07-04 03:25:42 -0700

    struct

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Structure in c projects
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+
+/**
+ * struct dog - dog definition
+ * @name: name of the dogo
+ * @age: age of the dogo
+ * @owner: owner of the dogo
+ *
+ * Description: defintion for structure of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */

ca40167fb3e5206749430f670835bc79d3dd8539
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-07-04 05:24:18 -0700

    null checked

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,7 +8,10 @@
  */
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\n", d->name);
-       	printf("Age: %.6f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	if (d != NULL)
+	{
+		printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
+		printf("Age: %.6f\n", d->age);
+		printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
+	}
 }

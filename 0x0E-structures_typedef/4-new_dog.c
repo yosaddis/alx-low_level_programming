00adb9190d6981d45ed3c6726c05428f5e820865
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-07-04 05:56:46 -0700

    fixing issues

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,6 @@
 #include"dog.h"
+#include<string.h>
+
 /**
  * new_dog - initializes new dog from struct
  * @name: name to assign
@@ -10,9 +12,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d = NULL;
 
-	d->name = name;
+	memcpy(&d->name,&name,sizeof(name));
 	d->age = age;
-	d->owner = owner;
+	memcpy(&d->owner, &owner,sizeof(owner));
 
 	return (d);
 }

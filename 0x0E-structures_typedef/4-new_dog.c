8c030a52ea8e99d77b56cf259b7addae11c46968
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-07-04 12:03:39 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,33 +15,31 @@ dog_t *new_dog(char *name, float age, char *owner)
 	int name_size, owner_size;
 	int i, j;
 
-	if (d != NULL || (name) || (owner))
+	if (d == NULL || !(name) || !(owner))
 	{
-		for (name_size = 0; name[name_size]; name_size++)
-			;
-		for (owner_size = 0; owner[owner_size]; owner_size++)
-			;
-		d->name = malloc(name_size + 1);
-		d->owner = malloc(owner_size + 1);
-		if ((d->name) || (d->owner))
-		{
-			for (i = 0; i < name_size; i++)
-				d->name[i] = name[i];
-			d->name[i] = '\0';
-			for (j = 0; j < owner_size; j++)
-				d->owner[j] = owner[j];
-			d->owner[j] = '\0';
-			d->age = age;
-			return (d);
-		}
-		else
-		{
-			free(d->name);
-			free(d->owner);
-			free(d);
-			return (NULL);
-		}
+		free(d);
+		return (NULL);
 	}
-	free(d);
-	return (NULL);
+	for (name_size = 0; name[name_size]; name_size++)
+		;
+	for (owner_size = 0; owner[owner_size]; owner_size++)
+		;
+	d->name = malloc(name_size + 1);
+	d->owner = malloc(owner_size + 1);
+
+	if (!(d->name) || !(d->owner))
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
+	for (i = 0; i < name_size; i++)
+		d->name[i] = name[i];
+	d->name[i] = '\0';
+	for (j = 0; j < owner_size; j++)
+		d->owner[j] = owner[j];
+	d->owner[j] = '\0';
+	d->age = age;
+	return (d);
 }

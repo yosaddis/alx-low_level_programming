52b1d309df62bf2edef7cd5144e525b9801ad629
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-07-15 13:39:14 -0700

    function called b4 main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include"lists.h"
+
+/**
+ * bmain - function called before main
+ * Return - no return
+ *
+ */
+
+void __attribute__((constructor)) bmain()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

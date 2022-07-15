958cbc3fe0a301115e7a539f604de99bdbfeb572
Yoseph Addisu <email.to.yoseph@gmail.com>
2022-07-15 12:45:17 -0700

    print linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,4 @@
-#include"list.h"
+#include"lists.h"
 
 /**
  * print_list - prints form linked list
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef LIST_H
-#define LIST_H
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -20,4 +20,4 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
-#endif /* LIST_H */
+#endif /* LISTS_H */
